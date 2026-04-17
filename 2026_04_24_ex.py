2e842f165007c89d375e1c9f2cfd6671c2aeb62a
mins2894 <mins520941@gmail.com>
2026-04-17 10:30:02 +0900

    중간고사 유?출

---
+++ 2026_04_24_ex.py
@@ -0,0 +1,18 @@
+# && <= and
+# || <= or
+# ^ <= xor
+print('') # sep, end <~ 사용해서 구분하는 문제 출제
+print(f'{a}') # format형식 및 변수를 print 내부에서 호출
+print('{}'.format(a,b))
+# int / int = float
+# 변수 규칙:대/소문자 구별, 숫자로 변수 시작, 특수문자 사용 X
+# 자료형 구별: none, int(), float(), str(), char()
+# 문자열 연결방법: 정수+문자=X / 문자+문자=I
+# input("") => str형태로 데이터 입력받음
+# 논리연산자, 산술연산자(+, =, *, /, //, %)
+# isalpha() isnumric(), isalnum() 알아놓기
+# 길이 구하는 함수 = len()
+# format() 함수 출력되는 결과값 확인
+# indexing 개념, slicing 개념 알기
+# input() 함수 코드를 설명할때에 에러가 발생하는 코드 수정하기
+# 복합대입연산자 += , -=, *=, /= 
